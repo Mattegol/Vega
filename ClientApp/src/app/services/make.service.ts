14aa8f3680eebab4f38acf7f261e3d886f66a38a
Mats Egnell <mattegol.egnell@gmail.com>
2019-05-01 14:54:23 +0200

    Creating MakeService

---
+++ ClientApp/src/app/services/make.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+// import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MakeService {
+
+constructor(private http: Http) { }
+
+getMakes() {
+  return this.http.get('/api/makes').pipe(map(res => res.json()));
+}
+
+}
