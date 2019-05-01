5fa2a2f136c47db75306505e6f443489bec3657b
Mats Egnell <mattegol.egnell@gmail.com>
2019-05-01 18:00:21 +0200

    Implementing cascading behaviour

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { MakeService } from './services/make.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
+import { MakeService } from './services/make.service';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -7,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
-  makes;
+  makes: any[];
+  models: any[];
+  vehicle: any = {};
 
   constructor(private makeService: MakeService) { }
 
   ngOnInit() {
-    this.makeService.getMakes().subscribe(makes => {
-      this.makes = makes;
-    });
+    this.makeService.getMakes().subscribe(makes =>
+      this.makes = makes);
+  }
+
+  onMakeChange() {
+    const selectedMake = this.makes.find(m => m.id === +this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
   }
 
 }
