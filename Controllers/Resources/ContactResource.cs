bb01ab3e2bdf37965c7b70f819d0e280816a1ba1
Mats Egnell <mattegol.egnell@gmail.com>
2019-05-02 13:38:47 +0200

    Creating API Resources

---
+++ Controllers/Resources/ContactResource.cs
@@ -0,0 +1,17 @@
+namespace vega.Controllers.Resources
+{
+    public class ContactResource
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Phone { get; set; }
+
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
