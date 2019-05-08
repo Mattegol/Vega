81e4b377d082c37dd8a6ecf4a4c463756e848226
Mats Egnell <mattegol.egnell@gmail.com>
2019-05-08 12:21:03 +0200

    Displaying toast Notifications

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ToastyModule } from 'ng2-toasty';
 
 import { VehicleService } from './services/vehicle.service';
 
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { AppErrorHandler } from './app.error-handler';
 
 @NgModule({
    declarations: [
@@ -28,6 +30,7 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
       HttpClientModule,
       HttpModule,
       FormsModule,
+      ToastyModule.forRoot(),
       RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
@@ -35,7 +38,9 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
       { path: 'vehicles/new', component: VehicleFormComponent }
     ])
   ],
-  providers: [VehicleService],
+  providers: [
+   { provide: ErrorHandler, useClass: AppErrorHandler },
+   VehicleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
